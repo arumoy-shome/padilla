204eac938d14d6b82a2fe75b8c3015ddda4e4d50
Arumoy Shome <arumoy.shome.work@gmail.com>
2016-04-22 11:02:06 -0400

    create views

---
+++ app.rb
@@ -0,0 +1,13 @@
+class App
+  def self.call(env)
+    path = env["REQUEST_PATH"]
+
+    case path
+    when "/"
+      RootController.new(env).show
+    else
+      body = "Not Found"
+      [404, {}, [body]]
+    end
+  end
+end
